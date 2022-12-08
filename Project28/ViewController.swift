588a23fdfbce5cee866a9095f6b9ec179b2e8739
EPJr <edwin.przezwiecki.jr@icloud.com>
2022-12-08 18:25:44 +0100

    Clean-ups

--- Project28/ViewController.swift
+++ Project28/ViewController.swift
@@ -9,11 +9,10 @@ import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var secret: UITextView!
     
+    @IBOutlet var secret: UITextView!
     /// Challenge 1:
     var doneButton: UIBarButtonItem!
-    
     /// Challenge 2:
     var password = ""
     
@@ -21,25 +20,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         title = "Nothing to see here"
-        
         /// Challenge 1:
         doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(saveSecretMessage))
         navigationItem.rightBarButtonItem = doneButton
         doneButton.isHidden = true
-        
         /// Challenge 2:
         load(key: "Password", object: &password)
-        
         /// Challenge 2:
         if password == "" {
+            
             let ac = UIAlertController(title: "Password", message: "Please set a backup password.", preferredStyle: .alert)
             ac.addTextField()
             ac.addAction(UIAlertAction(title: "Save", style: .default) { [weak self, weak ac] _ in
+                
                 guard let password = ac?.textFields?[0].text else { return }
+                
                 self?.save(password, key: "Password")
                 self?.password = password
             })
             ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            
             present(ac, animated: true)
         }
         
@@ -50,10 +50,12 @@ class ViewController: UIViewController {
     }
     
     @IBAction func authenticateTapped(_ sender: Any) {
+        
         let context = LAContext()
         var error: NSError?
         
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
             let reason = "Identify yourself!"
             
             context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
@@ -66,12 +68,15 @@ class ViewController: UIViewController {
                         let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified. Please use your password.", preferredStyle: .alert)
                         ac.addTextField()
                         ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self] _ in
+                            
                             guard let passwordString = ac.textFields?[0].text else { return }
+                            
                             if passwordString == self?.password {
                                 self?.unlockSecretMessage()
                             } else {
                                 let ac = UIAlertController(title: "Wrong password", message: "Sorry, the secret message can not be unlocked.", preferredStyle: .alert)
                                 ac.addAction(UIAlertAction(title: "OK", style: .default))
+                                
                                 self?.present(ac, animated: true)
                             }
                         })
@@ -85,26 +90,31 @@ class ViewController: UIViewController {
             let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication. Please use your password.", preferredStyle: .alert)
             ac.addTextField()
             ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self] _ in
+                
                 guard let passwordString = ac.textFields?[0].text else { return }
+                
                 if passwordString == self?.password {
                     self?.unlockSecretMessage()
                 } else {
                     let ac = UIAlertController(title: "Wrong password", message: "Sorry, the secret message can not be unlocked.", preferredStyle: .alert)
                     ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    
                     self?.present(ac, animated: true)
                 }
             })
             ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            
             self.present(ac, animated: true)
         }
     }
     
     @objc func saveSecretMessage() {
-        guard secret.isHidden == false else { return }
         
+        guard secret.isHidden == false else { return }
         /// Challenge 2:
-        //KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+//      KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
         save(secret.text, key: "SecretMessage")
+        
         secret.resignFirstResponder()
         secret.isHidden = true
         /// Challenge 1:
@@ -119,21 +129,23 @@ class ViewController: UIViewController {
     }
     
     func unlockSecretMessage() {
+        
         secret.isHidden = false
         /// Challenge 1:
         doneButton.isHidden = false
-        title = "Secret stuff!"
         
+        title = "Secret stuff!"
         /// Optional:
-        //secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
+        
+//      secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
         
         /* if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
             secret.text = text
         } */
+        
         /// Challenge 2:
         load(key: "SecretMessage", object: &secret.text)
     }
-    
     /// Challenge 2:
     func load(key: String, object: inout String) {
         if let text = KeychainWrapper.standard.string(forKey: key) {
@@ -142,6 +154,7 @@ class ViewController: UIViewController {
     }
     
     @objc func adjustForKeyboard(notification: Notification) {
+        
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
         
         let keyboardScreenEndFrame = keyboardValue.cgRectValue
